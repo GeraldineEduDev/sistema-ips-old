236d2a3f52131eeb962d860383b08cb77c488cfc
GeraldineEduDev <geraldine_tello@soy.sena.edu.co>
2024-09-10 10:34:53 -0500

    Mejoras en el layout

---
+++ resources/views/biologicos/index.blade.php
@@ -0,0 +1,23 @@
+@extends('components.layout')
+
+@section('name')
+    Biológicos | Inicio
+@endsection
+
+@section('content')
+    <h1 class="text-center">Biológicos</h1>
+
+    <table class="table">
+        <thead>
+            <th>Id</th>
+            <th>Nombre</th>
+            <th>Tipo</th>
+            <th>Precio</th>
+            <th>Presentación</th>
+            <th>Marca</th>
+            <th>Laboratorio</th>
+            <th>Fecha Expiración</th>
+            <th>Lote</th>
+        </thead>
+    </table>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,11 @@ use App\Http\Controllers\PatientsController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('index');
+    return view('components.layout');
+});
+
+Route::get('/biologicos', function () {
+    return view('biologicos.index');
 });
 
 Route::resource('patients', PatientsController::class);
